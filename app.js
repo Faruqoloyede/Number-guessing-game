b3a7cde3b8858a12b3783fc1443f34afc300b572
Faruqoloyede <oloyedefaruq2@gmail.com>
2022-11-09 20:32:28 +0100

    initial

---
+++ app.js
@@ -0,0 +1,20 @@
+const answer = Math.floor(Math.random() * 10 + 1);
+
+let guesses = 0;
+
+document.getElementById("btn").onclick = function(){
+    let guess = document.getElementById("guess").value;
+    guesses++;
+
+    if(guess == answer){
+        document.getElementById("message").innerHTML = (`${answer} Is the number it took you ${guesses} guesses`);
+    }
+    else if(guess < answer){
+        document.getElementById("msg").innerHTML = ("Too small!");
+        
+    }
+    else{
+        document.getElementById("msg").innerHTML = ( "Too large");
+        
+    }
+}
